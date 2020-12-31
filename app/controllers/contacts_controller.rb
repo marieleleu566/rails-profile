15966e16de9fd5fa4c6dac5e3be2318a08eb94d2
Marie Leleu <marieleleu@icloud.com>
2020-12-31 17:51:41 +0100

    started home page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = nil
+      # flash.now[:error] = nil
       redirect_to root_path, notice: 'Message sent successfully'
     else
       flash.now[:error] = 'Cannot send message'
