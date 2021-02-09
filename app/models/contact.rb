2397296f6489e226ad6dd3088ba75ca5873c6221
Marie Leleu <marieleleu@icloud.com>
2021-02-09 19:22:07 +0100

    test

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,7 @@ class Contact < MailForm::Base
     {
       #this is the subject for the email generated, it can be anything you want
       subject: "My Contact Form",
-      to: 'leleu.a.marie@gmail.com',
+      to: 'mariegoestocollege@gmail.com',
       from: %("#{name}" <#{email}>)
       #the from will display the name entered by the user followed by the email
     }
